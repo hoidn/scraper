aaf91100fc6416f99c46d41fabb41d6b2a19af18
oliver <oliver@olivers-MBP.hsd1.wa.comcast.net>
2016-07-12 23:04:21 -0700

    Added function to get second-by-second time series

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,8 +1,13 @@
 from .indices import sp500
 import errno    
-import os
 import time
+
 import pandas as pd
+import os
+from StringIO import StringIO
+
+# TODO: move this to config.py
+data_prefix = os.environ['HOME'] + '/Dropbox/projects/financial/data_scraper/data'
 
 try:
     # py3
@@ -52,3 +57,54 @@ def autodl_symbol(symbol):
         f.write(_request(symbol))
     return path
 
+
+def read_csv(path):
+    """
+    Load data from a single csv file, corresponding to
+    prices for one ticker during a single trading day.
+    """
+    with open(path) as f:
+        full = f.read()
+    header = 'Timestamp,close,high,low,open,volume\n'
+    return header + '\n'.join(full.split('\n')[17:])
+
+class StockDay:
+    def __init__(self, path):
+        self.time = self.path_to_timestamp(path)
+        self.ticker = self.path_to_ticker(path)
+        self.path = path
+        
+    def load(self):
+        return pd.read_csv(StringIO(read_csv(self.path)))
+
+    @staticmethod
+    def path_to_timestamp(path):
+        fname = os.path.basename(path)
+        time_uct = float(fname.split('_')[0])
+        return pd.Timestamp.utcfromtimestamp(time_uct)
+    
+    @staticmethod
+    def path_to_ticker(path):
+        fname = os.path.basename(path)
+        return fname.split('_')[1]
+    
+def merge_stockdays(stockdays):
+    """
+    Merge a sequence of StockDay instances,
+    returning a pandas dataframe.
+    """
+    return pd.concat([s.load() for s in stockdays])
+    
+def get_ticker_stocks(ticker, prefix = data_prefix):
+    """Return a list of StockDay instances for the given stock ticker and data directory prefix"""
+    from glob import glob
+    paths = glob(prefix + '/%s/*_*csv' % ticker)
+    return map(StockDay, paths)
+
+
+def get_time_series(ticker, start, end):
+    def stock_matches(stock):
+        return (stock.time > start) and (stock.time < end)
+    merged = merge_stockdays(filter(stock_matches, get_ticker_stocks(ticker)))
+    # TODO: abstraction violation?
+    return merged.drop_duplicates(subset = 'Timestamp')
